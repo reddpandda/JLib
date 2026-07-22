571d7a239c55287b18f3408d6996b7a684d17230
reddpandda <jakelley1994@gmail.com>
2026-07-22 17:57:50 -0500

    Add missing newline at end of userscript file

--- Examples/Example dashboard userscript.user.js
+++ Examples/Example dashboard userscript.user.js
@@ -2,6 +2,7 @@
 // @name         JLib Example — Dashboard (2+ modules)
 // @namespace    reddpandda
 // @version      3.0.0
+// @match        *://*/*
 // @grant        GM_getValue
 // @grant        GM_setValue
 // @grant        GM_registerMenuCommand
